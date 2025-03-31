603bf3daf01b318487a66e98beef35a8d2a03172
yaroslavn <yaroslav.n@open-tag.com>
2025-03-31 12:16:58 +0300

    Task: created REST API controllers for genres, genres/{id}/movie, movies and movies{id}.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\GenreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('genres', [GenreController::class, 'index']);
+Route::get('genres/{id}/movies', [GenreController::class, 'movies']);
+Route::get('movies', [MovieController::class, 'index']);
+Route::get('movies/{id}', [MovieController::class, 'show']);
